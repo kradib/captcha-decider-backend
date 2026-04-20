cb4063be44d8d5c294675ffc90cfc8dd9e6fb0fb
kradib <radibkar32@gmail.com>
2026-04-20 22:06:14 +0530

    fixed test

--- src/test/java/com/gmu/pragalv/capstone/model/CapstoneApplicationTests.java
+++ src/test/java/com/gmu/pragalv/capstone/model/CapstoneApplicationTests.java
@@ -1,9 +1,9 @@
-package com.gmu.pragalv.capstone;
+package com.gmu.pragalv.capstone.model;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CapstoneApplication.class)
 class CapstoneApplicationTests {
 
 	@Test
